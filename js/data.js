71e71884c97e31c67dc079d172ccbc90cc65db90
yonseiarts <ehrha44@gmail.com>
2025-06-22 16:46:26 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,33 +2,40 @@
 
 // 1. 히어로 섹션 콘텐츠
 const heroContent = {
-  type: 'video', // 'video' 또는 'image' 중 선택
-  src: 'videos/hero-video.mp4', // 사용자님의 비디오 경로
-  title: 'NEON', // 원하는 제목으로 변경 가능
-  subtitle: 'FILM COLLECTION' // 원하는 부제로 변경 가능
+  type: 'video',
+  src: 'videos/hero-video.mp4',
+  title: 'NEON',
+  subtitle: 'FILM COLLECTION'
 };
 
-
-// 2. 영화 그리드 콘텐츠
+// 2. 영화 그리드 콘텐츠 (상세 페이지를 위한 정보 추가)
 const filmData = [
   {
-    id: 'film1',
+    id: 'anora',
     poster: 'https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg',
-    title: 'ANORA'
+    title: 'ANORA',
+    trailerId: 'sS_8B-H2s6w',
+    synopsis: 'A young Brooklyn exotic dancer\'s fairytale marriage to the son of a Russian oligarch is shattered when his parents arrive in New York to force an annulment, sending her on a perilous journey to secure her future.'
   },
   {
-    id: 'film2',
+    id: 'parasite',
     poster: 'https://image.tmdb.org/t/p/w500/sEaC3U1aMybB0n0fL5eia2Sman2.jpg',
-    title: 'PARASITE'
+    title: 'PARASITE',
+    trailerId: '5xH0HfJHsaY',
+    synopsis: 'Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.'
   },
   {
-    id: 'film3',
+    id: 'portrait',
     poster: 'https://image.tmdb.org/t/p/w500/vqzNJd9oGfl9eS2N2K1hTOY_o3z.jpg',
-    title: 'PORTRAIT OF A LADY ON FIRE'
+    title: 'PORTRAIT OF A LADY ON FIRE',
+    trailerId: 'R-_b3aT_hEg',
+    synopsis: 'On an isolated island in Brittany at the end of the eighteenth century, a female painter is obliged to paint a wedding portrait of a young woman.'
   },
   {
-    id: 'film4',
+    id: 'triangle',
     poster: 'https://image.tmdb.org/t/p/w500/b34S25N6yT1pB0nS6wTJwT9MniG.jpg',
-    title: 'TRIANGLE OF SADNESS'
+    title: 'TRIANGLE OF SADNESS',
+    trailerId: 'VDvfAU_FACo',
+    synopsis: 'A cruise for the super-rich sinks, leaving survivors, including a celebrity model couple, trapped on an island.'
   },
 ];
